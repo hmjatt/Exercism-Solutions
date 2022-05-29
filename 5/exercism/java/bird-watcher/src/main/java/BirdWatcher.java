0dbdec7b5130bbd523763e730d6fb2ecfc4529ab
hmjatt <hmjatt@gmail.com>
2022-05-28 18:51:47 -0600

    added bird-watcher exercise

---
+++ 5/exercism/java/bird-watcher/src/main/java/BirdWatcher.java
@@ -0,0 +1,35 @@
+
+class BirdWatcher {
+    private final int[] birdsPerDay;
+
+    public BirdWatcher(int[] birdsPerDay) {
+        this.birdsPerDay = birdsPerDay.clone();
+    }
+
+    public int[] getLastWeek() {
+        int[] lastWeeksCount = {0, 2, 5, 3, 7, 8, 4};
+        return lastWeeksCount;
+    }
+
+    public int getToday() {
+        BirdWatcher birdCount = new BirdWatcher(getLastWeek);
+        int todayCount = birdCount[-1];
+        return todayCount;
+    }
+
+    public void incrementTodaysCount() {
+        throw new UnsupportedOperationException("Please implement the BirdCount.incrementTodaysCount() method");
+    }
+
+    public boolean hasDayWithoutBirds() {
+        throw new UnsupportedOperationException("Please implement the BirdCount.hasDayWithoutBirds() method");
+    }
+
+    public int getCountForFirstDays(int numberOfDays) {
+        throw new UnsupportedOperationException("Please implement the BirdCount.getCountForFirstDays() method");
+    }
+
+    public int getBusyDays() {
+        throw new UnsupportedOperationException("Please implement the BirdCount.getBusyDays() method");
+    }
+}
