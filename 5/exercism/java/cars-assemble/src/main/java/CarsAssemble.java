95e867f79d8e9cd38cd29130ce7241e41ec220fa
hmjatt <hmjatt@gmail.com>
2022-06-02 23:52:58 -0600

    completed CarsAssemble Exercise

--- 5/exercism/java/cars-assemble/src/main/java/CarsAssemble.java
+++ 5/exercism/java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,25 @@
 public class CarsAssemble {
 
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the AssemblyLine.productionRateperHour() method");
+
+        double ratePerHour;
+
+        if (speed >= 1 && speed <= 4) {
+            ratePerHour = speed * 221 * 1.0;
+        } else if (speed >= 5 && speed <=8) {
+            ratePerHour = speed * 221 * 0.9;
+        } else if (speed == 9) {
+            ratePerHour = speed * 221 * 0.8;
+        } else if (speed == 10) {
+            ratePerHour = speed * 221 * 0.77;
+        } else {
+            ratePerHour = 0.0;
+        }
+        return ratePerHour;
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the AssemblyLine.workingItemsPerMinute() method");
+        int workingItems = ((int)productionRatePerHour(speed)) / 60;
+        return workingItems;
     }
 }
