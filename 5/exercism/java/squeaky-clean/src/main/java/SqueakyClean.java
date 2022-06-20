1a08362fd65a217f3eeef6acc832208bc88cfdfd
hmjatt <hmjatt@gmail.com>
2022-06-19 21:00:46 -0600

    SqueakyClean passed 4 out of 10 tests

--- 5/exercism/java/squeaky-clean/src/main/java/SqueakyClean.java
+++ 5/exercism/java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,22 @@
 class SqueakyClean {
     static String clean(String identifier) {
-        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+        // char [] cleanIt = identifier.toCharArray();
+        // // if(java.lang.Character.isWhitespace(identifier) == true) {
+        // //     return
+        // // }
+        // char currentChar, changedChar;
+        // for (int i = 0; i < cleanIt.length; i++) {
+  
+        //     // accessing each element of array
+        //     currentChar = cleanIt[i];
+        //     if(java.lang.Character.isWhitespace(currentChar) == true) {
+        //         changedChar = currentChar.toString
+        //     }
+        // }
+
+    
+        String removedSpaces = identifier.replaceAll("\\s+","_");
+
+        return removedSpaces;
     }
 }
