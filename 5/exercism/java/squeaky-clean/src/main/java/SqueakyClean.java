39347a96f72017f2462bad7c88e84552683562b5
hmjatt <hmjatt@gmail.com>
2022-06-20 11:17:24 -0600

    SqueakyClean exercise updated

--- 5/exercism/java/squeaky-clean/src/main/java/SqueakyClean.java
+++ 5/exercism/java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -11,7 +11,23 @@ class SqueakyClean {
                 s = String.valueOf(chars[i]);
                 s = s.replace(s, "CTRL");
                 sb.append(s);
-            } else {
+            } else if(chars[i] == "-".charAt(0)){
+                // String s1 =  String.valueOf(chars[i]);
+                // String s2 = String.valueOf(chars[i+1]);
+                // String s4 = s2.toUpperCase();
+                // String s3 = s1 + s2;
+                // s = identifier;
+                // s = identifier.replaceAll(s3,(s2.toUpperCase()));
+                // s = s.replace(s1, "");
+                // s = s.replace(s1, "");
+                // s = s.replace(s3, "");
+
+        
+                sb.append(s);
+             
+                
+            } 
+            else {
                 sb.append(chars[i]);
             }
         }
